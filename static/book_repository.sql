5b46a458e7c73123264b11abf33ae18a13c6756e
Phuc Tran <contact@phuctrancs.info>
2015-05-27 17:50:17 +0700

    Add database file

---
+++ static/book_repository.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 27, 2015 at 12:49 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `book_repository`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE IF NOT EXISTS `books` (
+`id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `url` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `author` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `contributor` int(11) NOT NULL,
+  `create_date` date NOT NULL,
+  `isbn` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `name`, `description`, `url`, `category`, `author`, `contributor`, `create_date`, `isbn`) VALUES
+(1, 'How to import contact from iOS device to android', 'How to import contact from iOS device to android book', 0, 1, '', 0, '0000-00-00', ''),
+(2, 'How to Install AppVN', 'The MIT License (MIT)\r\n\r\nCopyright (c) 2014 - 2015, British Columbia Institute of Technology\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy\r\nof this software and associated documentation files (the "Software"), to deal\r\nin the Software without restriction, including without limitation the rights\r\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\ncopies of the Software, and to permit persons to whom the Software is\r\nfurnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in\r\nall copies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\r\nTHE SOFTWARE.', 0, 2, '', 0, '0000-00-00', ''),
+(3, 'Seed of IU', 'see of IU description', 0, 0, 'Tran Thanh Phuc', 0, '0000-00-00', '1234567');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_cate`
+--
+
+CREATE TABLE IF NOT EXISTS `book_cate` (
+`id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `book_cate`
+--
+
+INSERT INTO `book_cate` (`id`, `name`) VALUES
+(1, 'Ebooks'),
+(2, 'Ebook'),
+(3, 'Work Document'),
+(4, 'Studying Resource');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_usrs`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_usrs` (
+`id` int(8) NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(60) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `status` varchar(8) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_usrs`
+--
+
+INSERT INTO `tbl_usrs` (`id`, `username`, `password`, `email`, `status`) VALUES
+(5, 'admin', '098f6bcd4621d373cade4e832627b4f6', 'admin@phuctrancs.info', 'active');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book_cate`
+--
+ALTER TABLE `book_cate`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_usrs`
+--
+ALTER TABLE `tbl_usrs`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `book_cate`
+--
+ALTER TABLE `book_cate`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tbl_usrs`
+--
+ALTER TABLE `tbl_usrs`
+MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
